f0428f4377c849d277c23f030910c266edd147c2
leafartech <119262053+leafartech@users.noreply.github.com>
2024-04-18 10:59:49 -0300

    utm added on kiwify

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,11 +1,20 @@
-import { ReactNode } from "react"
+'use client'
+import { ReactNode, useEffect, useState } from "react"
 
 interface ButtonProps {
     children: ReactNode
 }
 
 export default function Button({children}: ButtonProps) {
+    const [ updatedUrl, setUpdatedUrl] = useState('')
+    useEffect(() => {
+        let url = 'https://pay.kiwify.com.br/yi7rtPT'
+        if (typeof window !== 'undefined') {
+            const params = window.top?.location.href.split("?")[1]?.split("&").join('&')
+            setUpdatedUrl(`${url}?${params}` )
+        }
+    }, [])
     return (
-        <a href="https://pay.kiwify.com.br/yi7rtPT" className="button text-center my-shadow">{children}</a>
+        <a href={updatedUrl} className="button text-center my-shadow">{children}</a>
     )
 }
